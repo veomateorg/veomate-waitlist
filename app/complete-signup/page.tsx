18a3b4886e8594ee3370aa0b34b31b4101b8bdec
SK Akram <skcodewizard786@gmail.com>
2025-12-07 08:13:38 +0000

    refactor: remove email display from complete signup page

--- app/complete-signup/page.tsx
+++ app/complete-signup/page.tsx
@@ -233,13 +233,6 @@ function CompleteSignupForm() {
                 </p>
               </div>
             </div>
-
-            <div className="hidden md:flex items-center gap-1 text-[10px] text-gray-500 bg-white/5 p-2.5 rounded-lg border border-white/5 mt-2 md:mt-0">
-              joined as{' '}
-              <span className="text-gray-300 font-medium truncate max-w-[140px]">
-                {email}
-              </span>
-            </div>
           </div>
 
           <div className="md:w-2/3 p-4 lg:p-8">
